b1a1649924aea4b914ecf944559ad23548539bf4
dmatos <dmatos88@gmail.com>
2021-08-29 12:46:57 -0300

    pjos

--- src/test/java/guru/springframework/sftpetclinic/SftPetClinicApplicationTests.java
+++ src/test/java/guru/springframework/sftpetclinic/SftPetClinicApplicationTests.java
@@ -2,6 +2,7 @@ package guru.springframework.sftpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.*;
 
 @SpringBootTest
 class SftPetClinicApplicationTests {
