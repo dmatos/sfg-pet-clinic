e00407ce9fd727649ac03bd6273d26e2f3d9e4c2
dmatos <dmatos88@gmail.com>
2021-09-11 20:39:07 -0300

    Closes #44

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/jpa/VetJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/jpa/VetJpaService.java
@@ -3,6 +3,7 @@ package guru.springframework.sfgpetclinic.services.jpa;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.repositories.VetRepository;
 import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -10,6 +11,7 @@ import java.util.Optional;
 import java.util.Set;
 
 @Service
+@Profile("springdatajpa")
 public class VetJpaService implements VetService {
 
     private final VetRepository vetRepository;
