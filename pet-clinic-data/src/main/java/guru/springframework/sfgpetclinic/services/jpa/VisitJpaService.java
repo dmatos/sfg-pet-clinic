4ddf1b2cd6efcb82c2e55255183517205efd5c81
dmatos <dmatos88@gmail.com>
2021-09-11 21:05:15 -0300

    Closes #50

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/jpa/VisitJpaService.java
@@ -0,0 +1,44 @@
+package guru.springframework.sfgpetclinic.services.jpa;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.repositories.VisitRepository;
+import guru.springframework.sfgpetclinic.services.VisitService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class VisitJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+            visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
